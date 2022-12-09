d41546dcc27188814d66747b031e8302d86d94d5
AmbroseUI <emmanueldan@uniuyo.ed.ng>
2022-12-09 14:59:07 -0800

    alphabets without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		if (k != 'e' && k != 'q')
+		{
+			putchar(k);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
