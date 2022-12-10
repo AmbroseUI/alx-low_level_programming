75c80af2341068b4a8c58fd03f83938e8273c53d
AmbroseUI <emmanueldan@uniuyo.ed.ng>
2022-12-09 16:57:20 -0800

    print numbers with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int k;
+
+	for (k = 0; k <= 9; k++)
+	{
+		putchar(k + '0');
+		if (k != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
