1cbf5e4c09283914375ccb1033dde4757797b3ed
AmbroseUI <94842636+AmbroseUI@users.noreply.github.com>
2022-12-10 05:49:46 +0100

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int k;
+	int j;
+
+	for (k = 0; k < 100; k++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (a < b)
+			{
+				putchar((k / 10) + 48);
+				putchar((k % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+
+				if (k != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
