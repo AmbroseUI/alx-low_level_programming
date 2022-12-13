197e694668d6ec54aefb2986c5dae958f190f740
AmbroseUI <94842636+AmbroseUI@users.noreply.github.com>
2022-12-13 23:46:21 +0100

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every second of jacks life
+ *
+ *Return: time
+ */
+void jack_bauer(void)
+{
+	int hrs;
+	int mins;
+
+	for (hrs = 0; hrs <= 23; hrs++)
+	{
+		for (mins = 0; mins <= 59; mins++)
+		{
+			_putchar(hrs / 10 + '0');
+			_putchar(hrs % 10 + '0');
+			_putchar(':');
+			_putchar(mins / 10 + '0');
+			_putchar(mins % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
