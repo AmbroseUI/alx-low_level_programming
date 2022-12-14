b38548a4891b183be63e45d2f1fe43e8f4315360
AmbroseUI <emmanueldan@uniuyo.ed.ng>
2022-12-13 16:11:33 -0800

    commit add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *add - compute the sum of two numbers.
+ *@num1: int value to be added
+ *
+ *@num2: int value to be added
+ *
+ * Return: int value
+ */
+
+int add(int num1, int num2)
+{
+
+	int sumVal;
+
+	sumVal = num1 + num2;
+	return (sumVal);
+
+}
+
+
