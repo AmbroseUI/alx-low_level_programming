4e0f27345e46754d92a32761e0abe89ae9c04285
AmbroseUI <94842636+AmbroseUI@users.noreply.github.com>
2022-12-14 15:33:28 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
+ * main - tests function that prints if integer is positive or negative
  *
- * * main - tests function that prints if integer is positive or negative
- *
- * * Return: 0
- *
- * */
+ * Return: 0
+ */
 
 int main(void)
 
@@ -18,6 +16,6 @@ int main(void)
 	i = 0;
 
 	positive_or_negative(i);
-
-        return (0);
+	
+	return (0);
 }
