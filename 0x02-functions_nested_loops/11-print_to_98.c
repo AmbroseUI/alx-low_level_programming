5cd9a18de8e34f2727b7b668d82f55e1edbe40c5
AmbroseUI <emmanueldan@uniuyo.ed.ng>
2022-12-13 16:45:37 -0800

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,23 +9,23 @@
 
 void print_to_98(int n)
 {
-  int k;
-  
-  if (n > 98)
-  {
-    for (k = n; k >= 98; k-- )
-      if (k == 98)
-        printf("%d", k);
-      else
-        printf("%d, ", k);
-  }
-  else
-  {
-    for (k = n; k <= 98; k++ )
-      if (k == 98)
-        printf("%d", k);
-      else
-        printf("%d, ", k);
-  }
-  printf("\n");
+	int k;
+
+	if (n > 98)
+	{
+		for (k = n; k >= 98; k--)
+			if (k == 98)
+				printf("%d", k);
+			else
+				printf("%d, ", k);
+	}
+	else
+	{
+		for (k = n; k <= 98; k++)
+			if (k == 98)
+				printf("%d", k);
+			else
+				printf("%d, ", k);
+	}
+	printf("\n");
 }
