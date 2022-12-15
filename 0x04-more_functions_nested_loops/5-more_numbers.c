5131f512f78a9c3f83ba77011e76e9a433cd2d2e
AmbroseUI <emmanueldan@uniuyo.ed.ng>
2022-12-15 07:54:39 -0800

    commit digit 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *more_numbers - Print 0 to 9 10times
+ *more_numbers - Print 0 to 14i 10times
  *
  *Return: Always 0 (Success)
  */
@@ -9,17 +9,21 @@
 void more_numbers(void)
 {
 
-	int k, i;
+	int k, i, j;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (k = 0; k <= 14; k++)
 		{
-			if (k <= 9)
-				_putchar(k + '0');
+			if (k > 9)
+				j = k / 10;
 			else
+				j = k;
+
+			_putchar(j + '0');
+
+			if (k > 9)
 			{
-				_putchar(k / 10 + '0');
 				_putchar(k % 10 + '0');
 			}
 		}
