6a521b28a240e26c65307d3f3f37996c18dbaed7
AmbroseUI <94842636+AmbroseUI@users.noreply.github.com>
2022-12-15 20:28:03 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * print_line - print lines on the terminal
- * @n: length of lines in "_" to be printed
+ * print_line - Print Lines
+ * @n: parameter to determine the length of the line
  */
 
 void print_line(int n)
 {
-	int start;
+	int k;
 
-	start = 1;
-
-	if (n > 0)
+	if (n  > 0)
 	{
-		while (start <= n)
+		for (k = 0; k <= n; k++)
 		{
 			_putchar('_');
-			start++;
 		}
-
 		_putchar('\n');
 	}
+
 	else
 	{
 		_putchar('\n');
