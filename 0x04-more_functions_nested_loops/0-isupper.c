62ee139d0040ade7d3679e5cf08acefebf9483cc
AmbroseUI <94842636+AmbroseUI@users.noreply.github.com>
2022-12-15 14:37:06 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+/**
+ *_isupper - Check if alphabets are lowercase.
+ *@c: int value to be checked
+ *
+ * Return: return 1 if true else 0.
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+/*	return (0);*/
+}
