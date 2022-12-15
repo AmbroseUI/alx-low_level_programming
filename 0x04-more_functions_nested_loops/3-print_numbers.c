2cf6515058078a2c3aaacaa5b39ed38859e6d501
AmbroseUI <94842636+AmbroseUI@users.noreply.github.com>
2022-12-15 15:42:53 +0100

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_numbers - Print o to 9
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+
+	int k;
+
+	for (k = 0; k <= 9; k++)
+	{
+		_putchar(k + '0');
+	}
+
+	_putchar("\n");
+
+	return (0);
+}
