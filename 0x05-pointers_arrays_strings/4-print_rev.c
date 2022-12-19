d676f44649ca7c494a433e9210a36c79d020c843
AmbroseUI <94842636+AmbroseUI@users.noreply.github.com>
2022-12-19 23:35:52 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,15 @@
 
 void print_rev(char *s)
 {
-	int k, len;
+	int k, len, len_1;
 	char f;
 
 	len = strlen(s);
 
 	if (len > 0)
 	{
-		for (k = len-1; k >= 0; k--)
+		len_1 = len - 1;
+		for (k = len_1; k >= 0; k--)
 		{
 			_putchar(s[k]);
 			f = '\n';
