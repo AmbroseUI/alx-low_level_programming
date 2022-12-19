fbda3650fd7390d30559c7c34ccaae757ce463ed
AmbroseUI <emmanueldan@uniuyo.edu.ng>
2022-12-19 11:40:11 -0800

    commit find length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - Find the length input string;
+ *@s: string value
+ *
+ * Return: int value
+ */
+
+int _strlen(char *s)
+{
+	int k;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+
+	}
+	return (k);
+}
