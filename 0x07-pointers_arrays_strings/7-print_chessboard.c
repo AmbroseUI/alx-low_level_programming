064f332ac0635c20aa65cf9fa90c3f479cebaf7e
AmbroseUI <emmanueldan@uniuyo.edu.ng>
2023-01-02 17:13:26 -0800

    commit Chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function to print chessboard
+ * @a: input array
+ *
+ * Return: Always 0 (suceess)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 8; n++)
+	{
+		for (m = 0; m < 8; m++)
+		{
+			_putchar(a[n][m]);
+		}
+		_putchar('\n');
+	}
+}
