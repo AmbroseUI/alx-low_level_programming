4ab2c6c16d69b3b3cd52b8f17f4fa2b25db0538a
AmbroseUI <emmanueldan@uniuyo.edu.ng>
2023-01-04 10:25:53 -0800

    commit task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Recursion function
+ * @s: input string
+ *
+ * Return: No return
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
