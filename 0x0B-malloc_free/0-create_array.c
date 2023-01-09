c0e61e4591e21f0549887b2ad962ce4b7c574b19
AmbroseUI <emmanueldan@uniuyo.edu.ng>
2023-01-09 15:07:38 -0800

    commit main.h file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * create_array - Memory function
+ * @size: Array size
+ * @c: input character
+ *
+ * Return: Return 1 always (success)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+}
